5f789275525942d18d13cc1937d0c1e367554203
Alderi-Junior <alderipasajunior@hotmail.com>
2025-01-21 15:48:12 -0300

    Project creation

---
+++ ms-event-manager/ms-event-manager/src/test/java/com/compass/mseventmanager/MsEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.mseventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
