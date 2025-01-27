dd85491e63000861d0e60995f2203d885b441e44
Alderi-Junior <alderipasajunior@hotmail.com>
2025-01-27 15:53:19 -0300

    feat: implementando operação POST /post-new-ticket

--- ms-ticket-manager/ms-ticket-manager/src/main/java/com/compass/msticketmanager/resources/TicketResource.java
+++ ms-ticket-manager/ms-ticket-manager/src/main/java/com/compass/msticketmanager/resources/TicketResource.java
@@ -3,15 +3,12 @@ package com.compass.msticketmanager.resources;
 import com.compass.msticketmanager.dto.TicketDto;
 import com.compass.msticketmanager.model.Ticket;
 import com.compass.msticketmanager.services.TicketService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,6 +34,13 @@ public class TicketResource {
         return ResponseEntity.ok().body(new TicketDto(ticket));
     }
 
+    @PostMapping(value = "post-new-ticket")
+    public ResponseEntity<Void> insert(@RequestBody TicketDto ticketDto) {
+        Ticket obj = ticketService.fromDTO(ticketDto);
+        obj = ticketService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getTicketId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 
 
 }
--- ms-ticket-manager/ms-ticket-manager/src/main/java/com/compass/msticketmanager/services/TicketService.java
+++ ms-ticket-manager/ms-ticket-manager/src/main/java/com/compass/msticketmanager/services/TicketService.java
@@ -1,5 +1,6 @@
 package com.compass.msticketmanager.services;
 
+import com.compass.msticketmanager.dto.TicketDto;
 import com.compass.msticketmanager.model.Ticket;
 import com.compass.msticketmanager.repositories.TicketRepository;
 import com.compass.msticketmanager.services.exception.ObjectNotFoundException;
@@ -22,5 +23,15 @@ public class TicketService {
     public Ticket findById(String id){
         Optional<Ticket> event = ticketRepository.findById(id);
         return event.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public Ticket insert(Ticket ticket) {
+        return ticketRepository.insert(ticket);
+    }
+
+    public Ticket fromDTO(TicketDto objDTO){
+        return new Ticket(objDTO.getTicketId(), objDTO.getCustomerName(), objDTO.getCpf(),
+                            objDTO.getCustomerMail(),objDTO.getEventId(), objDTO.getEventName(),
+                            objDTO.getBRLamount(), objDTO.getUSDamount());
     }
 }
